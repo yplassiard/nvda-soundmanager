d7d62d9a183615581c083cea6ce251b4f47c7300
Yannick Plassiard <plassiardyannick@gmail.com>
2019-05-17 10:15:13 +0200

    Fixes tabs issues

--- addon/globalPlugins/soundmanager/__init__.py
+++ addon/globalPlugins/soundmanager/__init__.py
@@ -11,94 +11,94 @@ from pycaw.pycaw import AudioUtilities
 del sys.path[-1]
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-    enabled = False
-    curAppName = None
+	enabled = False
+	curAppName = None
 
 
-    def sayFocusedApp(self, session):
-        name = None
-        try:
-            name = session.getDisplayName()
-        except:
-            pass
-        if name is None:
-            name = session.Process.name().replace(".exe", "")
-        ui.message(name)
+	def sayFocusedApp(self, session):
+		name = None
+		try:
+			name = session.getDisplayName()
+		except:
+			pass
+		if name is None:
+			name = session.Process.name().replace(".exe", "")
+		ui.message(name)
 
-    def script_volumeUp(self, gesture):
-        session,volume = self.findSessionByName(self.curAppName)
-        if session == None and self.curAppName is not None:
-            tones.beep(200, 500)
-            return
-        volume.SetMasterVolume(volume.GetMasterVolume() + 0.025, None)
+	def script_volumeUp(self, gesture):
+		session,volume = self.findSessionByName(self.curAppName)
+		if session == None and self.curAppName is not None:
+			tones.beep(200, 500)
+			return
+		volume.SetMasterVolume(volume.GetMasterVolume() + 0.025, None)
 
-    def script_volumeDown(self, gesture):
-        session,volume = self.findSessionByName(self.curAppName)
-        if session == None and self.curAppName is not None:
-            tones.beep(200, 500)
-            return
-        volume.SetMasterVolume(volume.GetMasterVolume() - 0.025, None)
+	def script_volumeDown(self, gesture):
+		session,volume = self.findSessionByName(self.curAppName)
+		if session == None and self.curAppName is not None:
+			tones.beep(200, 500)
+			return
+		volume.SetMasterVolume(volume.GetMasterVolume() - 0.025, None)
 
-    def script_nextApp(self, gesture):
-        audioSessions = AudioUtilities.GetAllSessions()
-        sessions = []
-        for session in audioSessions:
-            if session.Process is not None:
-                sessions.append(session)
-        newSession = None
-        idx = 0
-        nrSessions = len(sessions)
-        while idx < nrSessions:
-            session = sessions[idx]
-            if self.curAppName == session.Process.name():
-                newSession = sessions[idx + 1] if idx + 1 < nrSessions else sessions[0]
-            idx += 1
-        if newSession == None:
-            newSession = sessions[0]
-        self.curAppName = newSession.Process.name()
-        self.sayFocusedApp(newSession)
+	def script_nextApp(self, gesture):
+		audioSessions = AudioUtilities.GetAllSessions()
+		sessions = []
+		for session in audioSessions:
+			if session.Process is not None:
+				sessions.append(session)
+		newSession = None
+		idx = 0
+		nrSessions = len(sessions)
+		while idx < nrSessions:
+			session = sessions[idx]
+			if self.curAppName == session.Process.name():
+				newSession = sessions[idx + 1] if idx + 1 < nrSessions else sessions[0]
+			idx += 1
+		if newSession == None:
+			newSession = sessions[0]
+		self.curAppName = newSession.Process.name()
+		self.sayFocusedApp(newSession)
 
-    def script_previousApp(self, gesture):
-        audioSessions = AudioUtilities.GetAllSessions()
-        sessions = []
-        for session in audioSessions:
-            if session.Process is not None:
-                sessions.append(session)
-        newSession = None
-        idx = 0
-        nrSessions = len(sessions)
-        while idx < nrSessions:
-            session = sessions[idx]
-            if self.curAppName == session.Process.name():
-                newSession = sessions[idx - 1]
-            idx += 1
-        if newSession == None:
-            newSession = sessions[0]
-        self.curAppName = newSession.Process.name()
-        self.sayFocusedApp(newSession)
+	def script_previousApp(self, gesture):
+		audioSessions = AudioUtilities.GetAllSessions()
+		sessions = []
+		for session in audioSessions:
+			if session.Process is not None:
+				sessions.append(session)
+		newSession = None
+		idx = 0
+		nrSessions = len(sessions)
+		while idx < nrSessions:
+			session = sessions[idx]
+			if self.curAppName == session.Process.name():
+				newSession = sessions[idx - 1]
+			idx += 1
+		if newSession == None:
+			newSession = sessions[0]
+		self.curAppName = newSession.Process.name()
+		self.sayFocusedApp(newSession)
 
-    def script_soundManager(self, gesture):
-        self.enabled = not self.enabled
-        if self.enabled is True:
-            tones.beep(660, 100)
-            self.bindGesture("kb:uparrow", "volumeUp")
-            self.bindGesture("kb:downarrow", "volumeDown")
-            self.bindGesture("kb:leftarrow", "previousApp")
-            self.bindGesture("kb:rightarrow", "nextApp")
-        else:
-            tones.beep(440, 100)
-            self.clearGestureBindings()
-            self.bindGestures(self.__gestures)
+	def script_soundManager(self, gesture):
+		self.enabled = not self.enabled
+		if self.enabled is True:
+			tones.beep(660, 100)
+			self.bindGesture("kb:uparrow", "volumeUp")
+			self.bindGesture("kb:downarrow", "volumeDown")
+			self.bindGesture("kb:leftarrow", "previousApp")
+			self.bindGesture("kb:rightarrow", "nextApp")
+		else:
+			tones.beep(440, 100)
+			self.clearGestureBindings()
+			self.bindGestures(self.__gestures)
 
-    def findSessionByName(self, name):
-        sessions = AudioUtilities.GetAllSessions()
-        for session in sessions:
-            if session.Process and session.Process.name() == name or name is None:
-                volume = session.SimpleAudioVolume
-                return session,volume
-        return None,None
+	def findSessionByName(self, name):
+		sessions = AudioUtilities.GetAllSessions()
+		for session in sessions:
+			if session.Process and session.Process.name() == name or name is None:
+				volume = session.SimpleAudioVolume
+				return session,volume
+		return None,None
 
-    __gestures = {
-        "kb:nvda+v": "soundManager",
-    }
+	__gestures = {
+		"kb:nvda+v": "soundManager",
+	}
 
