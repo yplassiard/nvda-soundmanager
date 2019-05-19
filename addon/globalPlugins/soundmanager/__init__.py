1d55a44dbf29319a4a64b3891183c580d61c04bb
Yannick Plassiard <plassiardyannick@gmail.com>
2019-05-19 17:36:24 +0200

    Some fixes

--- addon/globalPlugins/soundmanager/__init__.py
+++ addon/globalPlugins/soundmanager/__init__.py
@@ -1,5 +1,9 @@
 # *-* coding: utf-8 *-*
-
+#addon/globalPlugins/sound-manager/__init__.py
+#A part of the NVDA Translate add-on
+#Copyright (C) 2018 Yannick PLASSIARD
+#This file is covered by the GNU General Public License.
+#See the file LICENSE for more details.
 
 import globalPluginHandler
 import tones
@@ -11,107 +15,101 @@ from pycaw.pycaw import AudioUtilities
 del sys.path[-1]
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-	scriptCategory = _("Sound Manager")
-	enabled = False
-	curAppName = None
+  scriptCategory = _("Sound Manager")
+  volumeChangeStep = 0.05
+  enabled = False
+  curAppName = None
 
+  
+  def getAppNameFromSession(self, session):
+    name = None
+    try:
+      name = session.getDisplayName()
+    except Exception as e:
+      name = session.Process.name().replace(".exe", "")
+    return name
 
-	def sayFocusedApp(self, session):
-		name = None
-		try:
-			name = session.getDisplayName()
-		except:
-			pass
-		if name is None:
-			name = session.Process.name().replace(".exe", "")
-		ui.message(name)
+  
+  def script_muteApp(self, gesture):
+    session,volume = self.findSessionByName(self.curAppName)
+    if session == None and self.curAppName is not None:
+      ui.message(_("Unable to retrieve current application."))
+      return
+    muted = volume.GetMute()
+    volume.SetMute(not muted, None)
+    if muted:
+      ui.message(_("{app} muted").format(app=self.getAppNameFromSession(session)))
+    else:
+      ui.message(_("{app} unmuted").format(app=self.getAppNameFromSession(session)))
 
-	def script_muteApp(self, gesture):
-		session,volume = self.findSessionByName(self.curAppName)
-		if session == None and self.curAppName is not None:
-			tones.beep(200, 500)
-			return
-		volume.SetMute(not volume.GetMute(), None)
+  def script_volumeUp(self, gesture):
+    self.changeVolume(self.volumeChangeStep)
+  def script_volumeDown(self, gesture):
+    self.changeVolume(-self.volumeChangeStep)
+  def changeVolume(self, volumeStep):
+    session,volume = self.findSessionByName(self.curAppName)
+    if session == None and self.curAppName is not None:
+      ui.message(_("Unable to retrieve current application."))
+      return
+    newVolume = volume.GetMasterVolume() + volumeStep
+    if volumeStep > 0 and newVolume > 1:
+      newVolume = 1.0
+    elif volumeStep < 0 and newVolume < 0:
+      newVolume = 0.0
+      
+    volume.SetMasterVolume(newVolume, None)
+    ui.message(_("{volume}%".format(volume=int(round(newVolume * 100)))))
+    
+  def cycleThroughApps(self, goForward):
+    audioSessions = AudioUtilities.GetAllSessions()
+    sessions = []
+    for session in audioSessions:
+      if session.Process is not None:
+	sessions.append(session)
+    newSession = None
+    idx = 0
+    nrSessions = len(sessions)
+    while idx < nrSessions:
+      session = sessions[idx]
+      if self.curAppName == session.Process.name():
+	if goForward:
+          newSession = sessions[idx + 1] if idx + 1 < nrSessions else sessions[0]
+        else:
+          newSession = sessions[idx - 1]
+      idx += 1
+    if newSession is None:
+      newSession = sessions[0]
+    self.curAppName = newSession.Process.name()
+    ui.message(self.getAppNameFromSession(newSession))
+  def script_nextApp(self, gesture):
+    self.cycleThroughApps(True)
+  def script_previousApp(self, gesture):
+    self.cycleThroughApps(False)
+  def script_soundManager(self, gesture):
+    self.enabled = not self.enabled
+    if self.enabled is True:
+      tones.beep(660, 100)
+      self.bindGesture("kb:uparrow", "volumeUp")
+      self.bindGesture("kb:downarrow", "volumeDown")
+      self.bindGesture("kb:leftarrow", "previousApp")
+      self.bindGesture("kb:rightarrow", "nextApp")
+      self.bindGesture("kb:m", "muteApp")
+    else:
+      tones.beep(440, 100)
+      self.clearGestureBindings()
+      self.bindGestures(self.__gestures)
+      
+  script_soundManager.__doc__ = _("""Toggle volume control adjustment on or off""")
 
-
-
-	def script_volumeUp(self, gesture):
-		session,volume = self.findSessionByName(self.curAppName)
-		if session == None and self.curAppName is not None:
-			tones.beep(200, 500)
-			return
-		volume.SetMasterVolume(volume.GetMasterVolume() + 0.025, None)
-
-	def script_volumeDown(self, gesture):
-		session,volume = self.findSessionByName(self.curAppName)
-		if session == None and self.curAppName is not None:
-			tones.beep(200, 500)
-			return
-		volume.SetMasterVolume(volume.GetMasterVolume() - 0.025, None)
-
-	def script_nextApp(self, gesture):
-		audioSessions = AudioUtilities.GetAllSessions()
-		sessions = []
-		for session in audioSessions:
-			if session.Process is not None:
-				sessions.append(session)
-		newSession = None
-		idx = 0
-		nrSessions = len(sessions)
-		while idx < nrSessions:
-			session = sessions[idx]
-			if self.curAppName == session.Process.name():
-				newSession = sessions[idx + 1] if idx + 1 < nrSessions else sessions[0]
-			idx += 1
-		if newSession == None:
-			newSession = sessions[0]
-		self.curAppName = newSession.Process.name()
-		self.sayFocusedApp(newSession)
-
-	def script_previousApp(self, gesture):
-		audioSessions = AudioUtilities.GetAllSessions()
-		sessions = []
-		for session in audioSessions:
-			if session.Process is not None:
-				sessions.append(session)
-		newSession = None
-		idx = 0
-		nrSessions = len(sessions)
-		while idx < nrSessions:
-			session = sessions[idx]
-			if self.curAppName == session.Process.name():
-				newSession = sessions[idx - 1]
-			idx += 1
-		if newSession == None:
-			newSession = sessions[0]
-		self.curAppName = newSession.Process.name()
-		self.sayFocusedApp(newSession)
-
-	def script_soundManager(self, gesture):
-		self.enabled = not self.enabled
-		if self.enabled is True:
-			tones.beep(660, 100)
-			self.bindGesture("kb:uparrow", "volumeUp")
-			self.bindGesture("kb:downarrow", "volumeDown")
-			self.bindGesture("kb:leftarrow", "previousApp")
-			self.bindGesture("kb:rightarrow", "nextApp")
-			self.bindGesture("kb:m", "muteApp")
-		else:
-			tones.beep(440, 100)
-			self.clearGestureBindings()
-			self.bindGestures(self.__gestures)
-
-	script_soundManager.__doc__ = _("""Toggle volume control adjustment on or off""")
-
-	def findSessionByName(self, name):
-		sessions = AudioUtilities.GetAllSessions()
-		for session in sessions:
-			if session.Process and session.Process.name() == name or name is None:
-				volume = session.SimpleAudioVolume
-				return session,volume
-		return None,None
-
-	__gestures = {
-		"kb:nvda+shift+v": "soundManager",
-	}
-
+  def findSessionByName(self, name):
+    sessions = AudioUtilities.GetAllSessions()
+    for session in sessions:
+      if session.Process and session.Process.name() == name or name is None:
+	volume = session.SimpleAudioVolume
+	return session,volume
+    return None,None
+    
+  __gestures = {
+    "kb:nvda+shift+v": "soundManager",
+  }
+  
