ba8aec5c5684aafeb8330eb83a00e3661700e315
Yannick Plassiard <plassiardyannick@gmail.com>
2019-07-04 17:33:44 +0200

    v2019.07

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Manages Windows and applications volumes directly within NVDA"""),
 	# version
-	"addon_version" : "2019.06.1",
+	"addon_version" : "2019.07",
 	# Author(s)
 	"addon_author" : u"Yannick PLASSIARD <podcastcecitek@gmail.com>, Danstiv <danstiv@blind.games>, Beqa Gozalishvili <beqaprogger@gmail.com>",
 	# URL for the add-on documentation support
