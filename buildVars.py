a903e342e245e48d08903173e13aaea180677075
Yannick Plassiard <plassiardyannick@gmail.com>
2023-07-26 13:49:34 +0200

    bump version to 2023.07.26 to be published within addon-datastore

--- buildVars.py
+++ buildVars.py
@@ -19,17 +19,17 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
     "addon_description" : _("""Manages Windows and applications volumes directly within NVDA"""),
 	# version
-	"addon_version" : "2021.06",
+	"addon_version" : "2023.07.26",
 	# Author(s)
 	"addon_author" : u"Yannick PLASSIARD <podcastcecitek@gmail.com>, Danstiv <danstiv@blind.games>, Beqa Gozalishvili <beqaprogger@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url" : None,
+	"addon_url" : "https://github.com/yplassiard/nvda-soundmanager",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3")
 	"addon_minimumNVDAVersion" : "2019.1",
 	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2021.1",
+	"addon_lastTestedNVDAVersion" : "2023.1",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : "stable"
 }
